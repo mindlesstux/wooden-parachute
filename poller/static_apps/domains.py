0118a97ea452f773fba76b72a6ea13f1e9166e2c
MindlessTux <mindlesstux@users.noreply.github.com>
2022-03-07 17:29:56 -0500

    Update domains.py

--- poller/static_apps/domains.py
+++ poller/static_apps/domains.py
@@ -1,3 +1,4 @@
+from dateutil import parser
 from dotenv import load_dotenv
 import logging
 import mysql.connector as database
@@ -21,7 +22,8 @@ except Error as e:
 
 # Select only the domains that have not been updated in the past 3hr
 
-statement = "SELECT * FROM staticapp_domains WHERE last_check <= (NOW() - INTERVAL 3 HOUR)"
+statement = "SELECT * FROM staticapp_domains WHERE last_check <= (NOW() - INTERVAL 3 HOUR) LIMIT 3"
+#statement = "SELECT * FROM staticapp_domains"
 cursor = db_connection.cursor()
 cursor.execute(statement)
 
@@ -38,20 +40,61 @@ for (domain_id, content_id, domain_name, registrar, date_created, date_updated, 
    if registrar != polled_registrar:
       to_update['registrar'] = "'%s'" % (polled_registrar)
 
-   # TODO: Date Created
-   # TODO: Date Updated
-   # TODO: Date Expire
+   # ---------------------------------------------------------------------------------------------
+   # Date Created
+   if type(whodata['creation_date']) is list:
+      tmp_date = sorted(list(set([str(x).lower() for x in whodata['creation_date']])))
+      polled_date_create = tmp_date[0]
+      del tmp_date
+   else:
+      polled_date_create = str(whodata['creation_date'])
+   
+   polled_date_create = parser.parse(polled_date_create)
+   print(polled_date_create)
+   if date_created != polled_date_create:
+      to_update['date_created'] = "'%s'" % (polled_date_create)
 
+   # ---------------------------------------------------------------------------------------------
+   # Date Updated
+   if type(whodata['updated_date']) is list:
+      tmp_date = sorted(list(set([str(x).lower() for x in whodata['updated_date']])))
+      polled_date_updated = tmp_date[0]
+      del tmp_date
+   else:
+      polled_date_updated = str(whodata['updated_date'])
+   
+   polled_date_updated = parser.parse(polled_date_updated)
+   print(polled_date_updated)
+   if date_created != polled_date_updated:
+      to_update['date_updated'] = "'%s'" % (polled_date_updated)
+
+   # ---------------------------------------------------------------------------------------------
+   # Date Expire
+   if type(whodata['expiration_date']) is list:
+      tmp_date = sorted(list(set([str(x).lower() for x in whodata['expiration_date']])))
+      polled_date_expire = tmp_date[0]
+      del tmp_date
+   else:
+      polled_date_expire = str(whodata['expiration_date'])
+   
+   polled_date_expire = parser.parse(polled_date_expire)
+   print(polled_date_expire)
+   if date_created != polled_date_expire:
+      to_update['date_expire'] = "'%s'" % (polled_date_expire)
+
+   # ---------------------------------------------------------------------------------------------
    # Nameservers
-   if type(whodata['status']) is list:
+   if type(whodata['name_servers']) is list:
       polled_nameservers = sorted(list(set([x.lower() for x in whodata['name_servers']])))
       polled_nameservers = ', '.join(polled_nameservers)
+      print(polled_nameservers)
    else:
       polled_nameservers = whodata['name_servers']
 
    if nameservers != polled_nameservers:
       to_update['nameservers'] = "'%s'" % (polled_nameservers)
 
+   # ---------------------------------------------------------------------------------------------
    # Status
    if type(whodata['status']) is list:
       polled_status = sorted(list(set([x.lower() for x in whodata['status']])))
@@ -68,6 +111,7 @@ for (domain_id, content_id, domain_name, registrar, date_created, date_updated, 
    if status != polled_status:
       to_update['status'] = "'%s'" % (polled_status)
 
+   # ---------------------------------------------------------------------------------------------
    # DNSSEC
    if type(whodata['dnssec']) is list:
       polled_dnssec = sorted(list(set([x.lower() for x in whodata['dnssec']])))
@@ -78,6 +122,7 @@ for (domain_id, content_id, domain_name, registrar, date_created, date_updated, 
    if dnssec != polled_dnssec:
       to_update['dnssec'] = "'%s'" % (polled_dnssec)
 
+   # ---------------------------------------------------------------------------------------------
    # Generate the SQL to update the row with the new data
    if to_update:
       print(to_update)
@@ -108,4 +153,3 @@ if check_sql is not []:
 
 # Close down connection
 db_connection.close()
-
